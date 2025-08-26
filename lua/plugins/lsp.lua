69244f621ac0b3d9c3c9b1b4da93620e1002066a
Fuad Daoud <fuad.daoud@icloud.com>
2025-08-26 12:06:34 +0300

    fix: using leader based gotos to be able to use it in the moonlander

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -21,13 +21,13 @@ return {
 
           map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
           map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-          map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+          map('<leader>gi', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
           map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
           map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
           map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
           map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
           map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
-          map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+          map('<leader>gd', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
           map('<leader>h', vim.lsp.buf.hover, '[H]over information')
 
           local client = vim.lsp.get_client_by_id(event.data.client_id)
